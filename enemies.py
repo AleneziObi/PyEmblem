9bce2495af507a2c8866bb5de9957595ca8fee4a
AleneziObi <obaid.alenezi@ucdenver.edu>
2025-04-30 14:59:18 -0600

    fixed enemy size

--- enemies.py
+++ enemies.py
@@ -5,7 +5,7 @@ class Enemy(Unit):
     def __init__(self, x, y, color, hp=5, attack=2, max_moves=3, attack_range=1):
         super().__init__(x, y, color, hp, attack, max_moves, attack_range)
         self.image = pygame.image.load("Assets/Theif.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (TILE_SIZE, TILE_SIZE))
+        self.image = pygame.transform.scale(self.image, (60, 80))
     
     def draw(self, surface, font):
         # Draw the enemy image instead of a colored rectangle.
